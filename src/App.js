870bd2bf63b5efff5e2bf525719fd9b7970c7f4a
Richar <ripupo88@gmail.com>
2020-07-07 04:48:03 +0100

    zx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import { HorarioScreen } from "./components/view/HorarioScreen";
+import { NavBar } from "./components/navBar/NavBar";
 
 function App() {
-    return <HorarioScreen />;
+    return (
+        <div>
+            <NavBar />
+            <HorarioScreen />
+        </div>
+    );
 }
 
 export default App;
